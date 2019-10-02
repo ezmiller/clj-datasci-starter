bcead58793505b7069d5799cac09008eab6f471a
Ethan Miller <ethanzanemiller@gmail.com>
2019-10-02 14:38:40 +0300

    Add libpython and pathera deps

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [alembic "0.3.2"]
+                 [cnuernber/libpython-clj "1.2"]
+                 [panthera "0.1-alpha.16"]
                  #_[cider/cider-nrepl "0.22.3"]]
   ;; :repl-options {:init-ns clj-datasci-starter.core}
   )
